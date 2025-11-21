e36809c741416a90078b26f6d693cee904bebd6b
arya pramudya <aryapramudya610@gmail.com>
2025-11-21 11:49:42 +0700

    Buat Kartu

---
+++ lib/card/card.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyCard extends StatelessWidget {
+  const MyCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Card"), centerTitle: true, actions: []),
+      body: Center(
+        child: Container(
+          child: Column(
+            children: [Card(color: Colors.green, child: Text("Card1"))],
+          ),
+        ),
+      ),
+    );
+  }
+}
